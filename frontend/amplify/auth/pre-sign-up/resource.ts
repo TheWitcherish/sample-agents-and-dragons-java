db70ed90d17c8757650ec6d37ed91e23f584ecb1
TheWitcherish <thewitcherish@gmail.com>
2026-05-17 18:16:45 +0300

    fix(auth): Update authentication permissions

--- frontend/amplify/auth/pre-sign-up/resource.ts
+++
@@ -1,9 +0,0 @@
-import { defineFunction } from '@aws-amplify/backend';
-
-export const preSignUp = defineFunction({
-  name: 'pre-sign-up',
-  
-  environment: {
-    ALLOW_DOMAIN: 'amazon.fr'
-  }
-});
--- frontend/amplify/auth/resource.ts
+++ frontend/amplify/auth/resource.ts
@@ -1,5 +1,4 @@
 import { defineAuth } from '@aws-amplify/backend';
-import { preSignUp } from './pre-sign-up/resource';
 
 /**
  * Define and configure your auth resource
@@ -9,8 +8,5 @@ export const auth = defineAuth({
   loginWith: {
     email: true,
   },
-  // triggers: {
-  //   // preSignUp
-  // },
   groups: ["ADMINS"]
 });
